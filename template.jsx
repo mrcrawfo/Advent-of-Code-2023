836797e0975f1907416624c22c101aecfa493698
Max Crawford <mrcrawfo@gmail.com>
2023-12-09 02:07:39 -0500

    feat: Adjusted template for initial test state

--- template.jsx
+++ template.jsx
@@ -8,24 +8,24 @@ const input2 = [
 
 ];
 
-const testArray1 = (inputArray) => {
+const test1 = (input) => {
 
 }
 
 console.log('Answer - Part 1 - Input 1');
-console.log(testArray1(input1));
+console.log(test1(input1));
 // 
-console.log('Answer - Part 1 - Input 2');
-console.log(testArray1(input2));
+//console.log('Answer - Part 1 - Input 2');
+//console.log(test1(input2));
 // 
 
-const testArray2 = (inputArray) => {
+const test2 = (input) => {
 
 }
 
-console.log('Answer - Part 2 - Input 1');
-console.log(testArray2(input1));
+//console.log('Answer - Part 2 - Input 1');
+//console.log(test2(input1));
 // 
-console.log('Answer - Part 2 - Input 2');
-console.log(testArray2(input2));
+//console.log('Answer - Part 2 - Input 2');
+//console.log(test2(input2));
 // 
